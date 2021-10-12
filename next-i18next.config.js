a891009f8f877e9104da1266b3d31f022d6c0e83
Luiz Spinelli <luiz.luchi@gmail.com>
2021-10-12 14:54:36 -0300

    i18n

---
+++ next-i18next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    i18n: {
+        defaultLanguage: "pt",
+        defaultNS: "common",
+        fallbackLng: "pt",
+        localePath: typeof window === "undefined" ? "public/locales" : "locales",
+        defaultLocale: "pt",
+        locales: ["pt", "es", "en"],
+        interpolation: {
+            escapeValue: false,
+        },
+    },
+    react: {
+        useSuspense: false,
+        wait: true,
+    },
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import { GetStaticProps } from "next";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
 export default function Home() {
-  return <div>Teste amplify</div>;
+  const { t } = useTranslation("home");
+  return <div>{t("home")}</div>;
 }
+
+export const getStaticProps: GetStaticProps<any> = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale!, ["home"])),
+  },
+});
